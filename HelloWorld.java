018b13cf59c0bd33068f645ccbf2398498732f61
gpm23 <112407186+gpm23@users.noreply.github.com>
2022-08-31 14:44:15 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package gpm23_HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	}
+
+}
